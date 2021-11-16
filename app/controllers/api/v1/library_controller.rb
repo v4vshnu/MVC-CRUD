0fabc9e313ce8dac0a1ff586136a2e29779ca043
Vishnu Sajith <me.vishnu24399@gmail.com>
2021-11-16 15:30:27 +0530

    Updated the Response messages

--- app/controllers/api/v1/library_controller.rb
+++ app/controllers/api/v1/library_controller.rb
@@ -3,32 +3,32 @@ module Api
         class LibraryController < ApplicationController
             def index
                 books = Book.order('id ASC');
-                render json: {status: 'SUCCESS', message:'Loaded Books', data:books},status: :ok
+                render json: {status: 'HTTP 200 OK', message:'All the books is in the library have been loaded', data:books},status: :ok
             end
             def show
                 book = Book.find(params[:id])
-                render json: {status: 'SUCCESS', message:'Loaded Book', data:book},status: :ok
+                render json: {status: 'HTTP 200 OK', message:'The book has been loaded', data:book},status: :ok
             end
             def create
                 book = Book.new(book_params)
                 if book.save
-                    render json: {status: 'SUCCESS', message:'Saved Books', data:book},status: :ok
+                    render json: {status: 'HTTP 200 OK', message:'New Book has been added to the Database', data:book},status: :ok
                 else
-                    render json: {status: 'ERROR', message:'Book not Saved', data:books.errors},status: :unprocessable_entity
+                    render json: {status: 'ERROR', message:'Error Adding the new book.', data:books.errors},status: :unprocessable_entity
                 end
             end
             def destroy
                 book = Book.find(params[:id])
                 book.destroy
-                render json: {status: 'SUCCESS', message:'Deleted Book', data:book},status: :ok
+                render json: {status: 'HTTP 200 OK', message:'The book has been deleted', data:book},status: :ok
 
             end
             def update
                book = Book.find(params[:id])
                if book.update(book_params)
-                render json: {status: 'SUCCESS', message:'Updated Book', data:book},status: :ok
+                render json: {status: 'HTTP 200 OK', message:'Details of the book has been updated', data:book},status: :ok
                else
-                render json: {status: 'ERROR', message:'Book not Updated', data:Book.errors},status: :unprocessable_entity
+                render json: {status: 'ERROR', message:'Error updating the details of the book', data:Book.errors},status: :unprocessable_entity
                end
                 
             end
